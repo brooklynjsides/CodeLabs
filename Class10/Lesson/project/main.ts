18d452f2477458ded2cf0dafb205329045a9bdd3
Brooklyn Sides <brooklynjsides@gmail.com>
2023-09-28 16:57:12 -0500

    9/28/23

--- Class10/Lesson/project/main.ts
+++ Class10/Lesson/project/main.ts
@@ -1,1 +1,42 @@
-console.log("hello");
+interface Student {
+  firstName: string;
+  grade: number;
+}
+
+interface Teacher {
+  firstName: string;
+  grade: number;
+  student: Student[];
+}
+
+class School<T> {
+  people: T[] = [];
+
+  addPeople(person: T) {
+    this.people.push(person);
+  }
+  showPeople() {
+    console.log(this.people);
+  }
+}
+
+const studentOne: Student = {
+  firstName: "Timmy",
+  grade: 12,
+};
+
+const teacherOne: Teacher = {
+  firstName: "John",
+  grade: 12,
+  student: [],
+};
+
+teacherOne.student = [studentOne];
+
+const newSchool = new School();
+
+newSchool.addPeople(studentOne);
+
+newSchool.addPeople(teacherOne);
+
+newSchool.showPeople();
---
+++ Class10/Project/script.js
@@ -0,0 +1,11 @@
+// const addLiBtn = document.querySelector(".addBtn");
+// const uList = document.querySelector(".TDLUL");
+
+// addLiBtn.addEventListener("click", () => {
+//   const lItem = document.createElement("li");
+//   const textArea = document.createElement("textarea");
+//   textArea.className = "tDLTextArea";
+
+//   lItem.appendChild(textArea);
+//   uList.appendChild(lItem);
+// });
